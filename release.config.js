753baf008dbd9afa9ebfc2eae24257f68821260e
HerbertJulio <herbert.julio@azion.com>
2023-02-13 07:33:21 -0300

    feat: add ci generate tags

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  branches: ['main', 'stage'],
+  analyzeCommits: {
+    preset: 'conventionalcommits',
+    parserOpts: {
+      headerPattern: /^(\[ENG-.*])?\s?(\w+):\s(.*)$/,
+      headerCorrespondence: ['scope', 'type', 'subject'],
+    },
+  },
+  generateNotes: {
+    preset: 'conventionalcommits',
+    parserOpts: {
+      headerPattern: /^(\[ENG-.*])?\s?(\w+):\s(.*)$/,
+      headerCorrespondence: ['scope', 'type', 'subject'],
+    },
+    options: {
+      preset: {
+        name: 'conventionalchangelog',
+        issuePrefixes: ['ENG-'],
+        issueUrlFormat: 'https://aziontech.atlassian.net/browse/{prefix}{id}',
+      },
+    },
+  },
+};
