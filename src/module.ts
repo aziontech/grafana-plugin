ad4e3b05a39e6fb7b4f7e2833c7cde1a66dc4af8
Herbert Vicente Cotta Julio <herbert.julio@azion.com>
2023-03-20 08:50:09 -0300

    Deploy 2023-03-20 (#17)
    * feat: first deploy the plugin for grafana labs
    ---------
    
    Co-authored-by: rodrigo.maria <rodrigo@rodrigomaria.com.br>

--- src/module.ts
+++ src/module.ts
@@ -1,11 +1,11 @@
 import { DataSourcePlugin } from '@grafana/data';
 import { DataSource } from './dataSource';
 import { ConfigEditor, QueryEditor } from './components';
-import { MyQuery, BasicDataSourceOptions } from './types';
+import { MyQuery } from './types';
 import { GraphQLAnnotationsQueryCtrl } from './GraphQLAnnotationsQueryCtrl';
 import { VariableQueryEditor } from './VariableQueryEditor';
 
-export const plugin = new DataSourcePlugin<DataSource, MyQuery, BasicDataSourceOptions>(DataSource)
+export const plugin = new DataSourcePlugin<DataSource, MyQuery>(DataSource)
   .setConfigEditor(ConfigEditor)
   .setAnnotationQueryCtrl(GraphQLAnnotationsQueryCtrl)
   .setQueryEditor(QueryEditor)
